d4c81f5ca65d198ae9a7302e31937c0b65cf69fc
stefan0092 <113347702+stefan0092@users.noreply.github.com>
2024-09-05 15:35:01 +0200

    step one

---
+++ main.js
@@ -0,0 +1,4 @@
+document.addEventListener("DOMContentLoaded", function() {
+    console.log("Die Seite wurde erfolgreich geladen.");
+    // Hier kannst du weitere JavaScript-Funktionen hinzufügen
+});
